7d82c331975c2f157a032df818c603eeed40e7f9
Daniel Souza avelino <danielavelino@ucl.br>
2022-11-29 00:30:35 -0300

    Desafio finalizado

---
+++ src/components/Albums.vue
@@ -0,0 +1,34 @@
+<template lang="pt-Br">
+  <div v-for="item in albuns">
+    <div>
+      <router-link :to="{name:'photos', params:{id: item.id}}">
+        Usuário: {{item.userId}} Título: {{ item.title}}
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios"
+import { ref, onMounted } from 'vue';
+
+export default {
+  name:"AlbumsTest",
+  setup(props){
+    const albuns = ref(props.album);
+
+    onMounted(()=>{
+      axios
+        .get("https://jsonplaceholder.typicode.com/albums")
+        .then((res)=>{
+          albuns.value = res.data;
+        })
+    })
+
+    return {  albuns  }
+  }
+}
+
+</script>
+<style lang="pt-Br">
+  
+</style>
---
+++ src/components/Photos.vue
@@ -0,0 +1,41 @@
+<template lang="pt-Br">
+  <div>
+    <button @click="back">Voltar</button>
+  </div>
+  <div>
+    <h2>Photos</h2>
+    <div v-for="image in photos">
+      <img :src="image.thumbnailUrl" :alt="image.title">
+      <p>{{image.title}}</p>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import { useRoute } from 'vue-router'
+import { onMounted, ref } from 'vue';
+
+export default {
+  name:"PhotosTest",
+  methods: {
+    back() {
+      this.$router.back();
+    }
+  },
+  setup(props) {    
+    const photos = ref(props.photos);
+    onMounted(()=>{
+      axios
+        .get(`https://jsonplaceholder.typicode.com/albums/${useRoute().params.id}/photos`)
+        .then((res)=>{
+          photos.value = res.data;
+        })
+    })
+    console.log(photos);
+    return {photos};
+  }
+}
+</script>
+<style lang="pt-Br">
+  
+</style>
